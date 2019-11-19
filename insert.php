71ad19234343d3148449b2456251fb088f334f21
dean.pektas27 <dean.pektas27@qmail.cuny.edu>
2019-11-18 22:23:01 -0500

    Updated email config for sponsors

--- insert.php
+++ insert.php
@@ -15,8 +15,6 @@ $usertable="sponsorform";
 
 $response_array['status'] = "";
 
-$response_array['status'] = "";
-
 $link= mysqli_connect($hostname,$username,$password,$dbname);
 
 //check connection
@@ -31,9 +29,24 @@ if(mysqli_query($link, $sql)) {
     header('Location:index.html');
     //$response_array['status'] = "success";
     //echo $response_array['status'];
+    $subject = "New DI Sponsor Request!";
+    
+    
+    $mail .="deanpektas12345@gmail.com";
+    $mail .="dean@diverseinfluencers.org";
+    
+    $headers .= "Reply-To: The Sender <sender@domain.com>\r\n"; 
+    $headers .= "Return-Path: The Sender <sender@domain.com>\r\n"; 
+    $headers .= "From: The Sender <sender@domain.com>\r\n";  
+    $headers .= "Organization: Sender Organization\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
+    $headers .= "X-Priority: 3\r\n";
+    $headers .= "X-Mailer: PHP". phpversion() ."\r\n" ;
+
     $msg = $firstname . " " . $lastname . " has requested to be a DI sponsor!\n\nInfo:\nCompany: " . $org . "\nEmail: " . $email . "\nPhone: " . $phone;
     $msg = wordwrap($msg,150);
-    mail("dean@diverseinfluencers.org","New DI Sponsor request!", $msg);
+    mail($mail , $subject, $msg, $headers);
     
 } else {
     echo "ERROR: Unable to execute :( ." . mysqli_error($link);
